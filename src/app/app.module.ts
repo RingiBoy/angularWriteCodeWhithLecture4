c822a379da513516325a2d3724d1e1b9bb416d7e
ringi <mitya.info@gmail.com>
2020-07-15 17:23:06 +0300

    all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './components/app/app.component';
+import {AppComponent} from './components/app/app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
-import {UserService} from './services/user.service';
-import { AllUserComponent } from './components/all-user/all-user.component';
+import {UserService} from './services/user/user.service';
+import {AllUserComponent} from './components/all-user/all-user.component';
+import {SingleUserComponent} from './components/single-user/single-user.component';
+import {UserResolveService} from './services/user/user-resolve.service';
+import {NexRouteService} from './services/nex-route.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AllUserComponent
+    AllUserComponent,
+    SingleUserComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'users', component: AllUserComponent}
+      {path: 'users', component: AllUserComponent,
+        resolve: {xxx: UserResolveService},
+        canActivate: [NexRouteService],
+        children: [
+          {path: ':id', component: SingleUserComponent}
+        ]
+      },
     ])
   ],
-  providers: [UserService],
+  providers: [UserService, UserResolveService, NexRouteService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/all-user/all-user.component.ts
+++ src/app/components/all-user/all-user.component.ts
@@ -1,21 +1,26 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from '../../models/User';
-import {UserService} from '../../services/user.service';
+import {UserService} from '../../services/user/user.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-all-user',
   templateUrl: './all-user.component.html',
   styleUrls: ['./all-user.component.css']
 })
-export class AllUserComponent implements OnInit {
+export class AllUserComponent {
 
   users: User[];
 
-  constructor(private userServ: UserService) {
-    userServ.getAllUsers().subscribe(value => this.users = value);
+  // constructor(private userServ: UserService, private router: Router) {
+  //   userServ.getAllUsers().subscribe(value => this.users = value);
+  // }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+    this.activatedRoute.data.subscribe(value => this.users = value.xxx);
   }
 
-  ngOnInit(): void {
-  }
 
+  toUserDetail(use: User): void {
+  this.router.navigate(['users', use.id], {state: {user: use}});
+  }
 }
