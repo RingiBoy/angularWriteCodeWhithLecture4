b5b369024d710e101ed51183bd948093be1eedca
ringi <mitya.info@gmail.com>
2020-07-18 19:28:11 +0300

    не получаю с сервиса комменты(ннн

---
+++ src/app/components/all-post/all-post.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Post} from '../../models/Post';
+import {PostService} from '../../services/post/post.service';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-all-post',
+  templateUrl: './all-post.component.html',
+  styleUrls: ['./all-post.component.css']
+})
+export class AllPostComponent {
+  posts: Post[];
+
+  constructor(private postService: PostService, private router: Router, private activatedRoute: ActivatedRoute) {
+    postService.getAllPost().subscribe(allPost => {
+      this.posts = allPost;
+
+
+    });
+  }
+
+
+  ShowComm(PostId: number) {
+    this.router.navigate(['posts', PostId, 'comments']);
+  }
+}
